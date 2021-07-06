e3096a6e2b4565e26cedda71859a21398b83c823
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-07-06 22:40:53 +0500

    feat: add statistics reset feature

--- english-for-kids/src/pages/Statistics/Statistics.tsx
+++ english-for-kids/src/pages/Statistics/Statistics.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ICardStats from '../../models/ICardStats';
 import './statistics.scss';
 import StatsTable from './table';
 
 const Statistics: React.FC = () => {
   const keys = Object.keys(localStorage);
-  const cardStatsArr: ICardStats[] = keys.reduce<ICardStats[]>((acc, key) => {
-    const word: ICardStats = JSON.parse(localStorage.getItem(key) || '');
-    acc.push(word);
-    return acc;
-  }, []);
+  const [cardStatsArr, setCardStatsArr] = useState<ICardStats[]>(
+    keys.reduce<ICardStats[]>((acc, key) => {
+      const word: ICardStats = JSON.parse(localStorage.getItem(key) || '');
+      acc.push(word);
+      return acc;
+    }, [])
+  );
+  // const cardStatsArr: ICardStats[] =
+  const resetStats = () => {
+    setCardStatsArr((prev) =>
+      prev.map<ICardStats>((_, index) => {
+        const currStats = JSON.parse(localStorage.getItem(keys[index]) || '');
+        const newStats = { ...currStats, correctClicks: 0, clicks: 0, wrongClicks: 0 };
+        localStorage.setItem(keys[index], JSON.stringify(newStats));
+        return newStats;
+      })
+    );
+  };
 
   return (
     <div className="statistics">
       <div className="container">
-        <button>reset</button>
+        <button onClick={resetStats}>reset</button>
         <button>train difficult</button>
         <StatsTable cardStatsArr={cardStatsArr} />
       </div>
