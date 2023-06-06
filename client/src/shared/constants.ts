e07e99e4bec51c79d1cb7be51edfbd5f3df913ea
Aziz <aziz.abdurahimov151002@gmail.com>
2023-06-06 19:10:57 +0500

    chore: change backend url

--- client/src/shared/constants.ts
+++ client/src/shared/constants.ts
@@ -10,7 +10,6 @@ export enum CardStatsProps {
   correctPersent = 'correctPersent'
 }
 
-// export const SERVER_URL = 'https://english-for-kids-project.herokuapp.com';
-export const SERVER_URL = 'http://localhost:3000';
+export const SERVER_URL = 'https://english-for-kids-server.onrender.com';
 export const DEFAULT_IMG_SRC =
   'https://res.cloudinary.com/dwx3ott96/image/upload/v1626625216/images/no-image_zb4hwr.png';
